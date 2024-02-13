09b287118122ab0ec7ce73b1256ce299a6ecd718
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-02-13 17:08:54 +0800

    fix diagnositc icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -254,7 +254,13 @@ return {
         },
         sections = {
           lualine_a = { "mode" },
-          lualine_b = { "branch", "diagnostics" },
+          lualine_b = {
+            "branch",
+            {
+              "diagnostics",
+              symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+            },
+          },
           lualine_c = { "filename" },
           lualine_x = { "filetype" },
           lualine_y = { LSP_status },
