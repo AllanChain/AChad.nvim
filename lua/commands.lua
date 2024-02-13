5d9e6f93c4d7c55352ec9315e0c7c64f052fc6db
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-02-13 12:25:42 +0800

    mason install all

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,8 +1,3 @@
--- mason, write correct names only
-vim.api.nvim_create_user_command("MasonInstallAll", function()
-  vim.cmd "MasonInstall css-lsp html-lsp lua-language-server typescript-language-server stylua prettier"
-end, {})
-
 -- setup colorcolumn based on editorconfig
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -23,7 +23,7 @@ return {
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
-    cmd = { "Mason", "MasonInstall" },
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
     opts = {
       ensure_installed = {
         "lua-language-server",
@@ -36,12 +36,25 @@ return {
         "shfmt",
         "shellcheck",
       },
+      PATH = "skip",
       github = {
         download_url_template = github .. "%s/releases/download/%s/%s",
       },
+      ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = "󰄳 ",
+          package_uninstalled = " 󰚌",
+        },
+      },
     },
-    config = function()
-      require("mason").setup()
+    config = function(_, opts)
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end
+      end, {})
+      require("mason").setup(opts)
     end,
   },
   {
