92453c54fc120c8302fc19af7f0b582d86a9cb1f
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-02-24 19:24:04 +0800

    direnv is preferred over guessing python path

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -88,26 +88,7 @@ setup("tsserver", {
   end,
 })
 
--- https://github.com/neovim/nvim-lspconfig/issues/500
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-  end
-  -- Find and use virtualenv via poetry in workspace directory.
-  local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
-  if match ~= "" then
-    local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
-    return path.join(venv, "bin", "python")
-  end
-  -- Fallback to system Python.
-  return exepath "python3" or exepath "python" or "python"
-end
-
 setup("pyright", {
-  before_init = function(_, config)
-    config.settings.python.pythonPath = get_python_path(config.root_dir)
-  end,
   settings = {
     python = { analysis = { typeCheckingMode = "off" } },
   },
