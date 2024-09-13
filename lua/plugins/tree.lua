7127053066eb2781fac8a0c44dc1b0ce510a5515
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-09-13 21:05:10 +0800

    switch to neo-tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,83 +1,21 @@
 return {
   {
-    "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    "nvim-neo-tree/neo-tree.nvim",
+    command = { "Neotree" },
+    branch = "v3.x",
     keys = {
-      { "<C-n>", "<cmd> NvimTreeToggle <CR>", desc = "Toggle tree" },
-      { "<leader>e", "<cmd> NvimTreeFocus <CR>", desc = "Focus tree" },
+      { "<C-n>", "<cmd> Neotree toggle <CR>", desc = "Toggle tree" },
+      { "<leader>e", "<cmd> Neotree focus <CR>", desc = "Focus tree" },
     },
     opts = {
-      filters = {
-        dotfiles = false,
-      },
-      disable_netrw = true,
-      hijack_netrw = true,
-      hijack_cursor = true,
-      hijack_unnamed_buffer_when_opening = false,
-      sync_root_with_cwd = true,
-      update_focused_file = {
-        enable = true,
-        update_root = false,
-      },
-      view = {
-        adaptive_size = false,
-        side = "left",
+      window = {
         width = 30,
-        preserve_window_proportions = true,
-      },
-      git = {
-        enable = false,
-        ignore = true,
-      },
-      filesystem_watchers = {
-        enable = true,
-      },
-      actions = {
-        open_file = {
-          resize_window = true,
-        },
-      },
-      renderer = {
-        root_folder_label = false,
-        highlight_git = false,
-        highlight_opened_files = "none",
-        indent_markers = {
-          enable = false,
-        },
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = false,
-          },
-          glyphs = {
-            default = "󰈚",
-            symlink = "",
-            folder = {
-              default = "",
-              empty = "",
-              empty_open = "",
-              open = "",
-              symlink = "",
-              symlink_open = "",
-              arrow_open = "",
-              arrow_closed = "",
-            },
-          },
-        },
       },
     },
-    config = function(_, opts)
-      require("nvim-tree").setup(opts)
-    end,
     dependencies = {
-      {
-        "nvim-tree/nvim-web-devicons",
-        config = function()
-          require("nvim-web-devicons").setup()
-        end,
-      },
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
     },
   },
 }
