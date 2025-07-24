c9910402f3a1d0186b6b42665481db8329d98b06
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-07-24 09:42:14 +0800

    fix header switch for clangd

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -3,7 +3,29 @@ return {
     local wk_ok, wk = pcall(require, "which-key")
     if wk_ok then
       wk.add({
-        { "gh", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Source/header" },
+        {
+          "gh",
+          function()
+            local method_name = "textDocument/switchSourceHeader"
+            if not client then
+              return vim.notify(
+                ("method %s is not supported by any servers active on the current buffer"):format(method_name)
+              )
+            end
+            local params = vim.lsp.util.make_text_document_params(bufnr)
+            client.request(method_name, params, function(err, result)
+              if err then
+                error(tostring(err))
+              end
+              if not result then
+                vim.notify "corresponding file cannot be determined"
+                return
+              end
+              vim.cmd.edit(vim.uri_to_fname(result))
+            end, bufnr)
+          end,
+          desc = "Source/header",
+        },
       }, { bufnr = bufnr })
     end
   end,
