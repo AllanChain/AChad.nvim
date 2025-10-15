fd2ed3c82d9654f7f6397950f58509b06d2a2dbf
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-10-15 10:55:01 +0800

    search git files using <leader>ff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,21 @@
+local is_inside_work_tree = {}
+
+local project_files = function()
+  local opts = {}
+
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+
+  if is_inside_work_tree[cwd] then
+    require("telescope.builtin").git_files(opts)
+  else
+    require("telescope.builtin").find_files(opts)
+  end
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -7,9 +25,7 @@ return {
     keys = {
       {
         "<leader>ff",
-        function()
-          require("telescope.builtin").find_files()
-        end,
+        project_files,
         desc = "Find files",
       },
       {
