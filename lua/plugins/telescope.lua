5c21db7c457d83ebdb21aafcc5c27a51fcf9b210
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2025-10-24 10:10:54 +0800

    glob hidden files tracked by git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ local project_files = function()
 
   local cwd = vim.fn.getcwd()
   if is_inside_work_tree[cwd] == nil then
-    vim.fn.system("git rev-parse --is-inside-work-tree")
+    vim.fn.system "git rev-parse --is-inside-work-tree"
     is_inside_work_tree[cwd] = vim.v.shell_error == 0
   end
 
@@ -31,7 +31,7 @@ return {
       {
         "<leader>fw",
         function()
-          require("telescope.builtin").live_grep()
+          require("telescope.builtin").live_grep { additional_args = { "--hidden", "--glob", "!.git" } }
         end,
         desc = "Find words",
       },
