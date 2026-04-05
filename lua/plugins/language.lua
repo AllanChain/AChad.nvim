d3a560f270e0c2f1c9b0ec9767cca1ee3839575e
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2026-04-05 10:50:42 +0800

    ignore error when treesitter not available

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -12,7 +12,7 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         group = vim.api.nvim_create_augroup("dotfiles_treesitter", { clear = true }),
         callback = function(args)
-          vim.treesitter.start(args.buf)
+          pcall(vim.treesitter.start, args.buf)
         end,
       })
     end,
